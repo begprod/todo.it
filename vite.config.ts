f2c930eac93e79bad60b6da437cf5c5e645f4545
Andrey Aratov <beg431@gmail.com>
2023-06-26 00:08:44 +0700

    updated vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,37 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  },
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        short_name: 'todo.it',
+        name: '',
+        description: '',
+        start_url: '.',
+        display: 'standalone',
+        orientation: 'portrait',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+        ],
+      },
+      devOptions: {
+        enabled: false,
+      }
+    }),
+  ],
+});
