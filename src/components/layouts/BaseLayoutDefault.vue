a30f5a7566ec904ae6aac84ef16721a8a0c9bf03
Andrey Aratov <beg431@gmail.com>
2023-08-17 20:36:29 +0700

    responsive base layout

--- src/components/layouts/BaseLayoutDefault.vue
+++ src/components/layouts/BaseLayoutDefault.vue
@@ -1,7 +1,9 @@
 <template>
   <BaseHeader />
 
-  <main class="container min-h-[calc(100vh-120px)] mx-auto pt-28 px-5 pb-10">
+  <main
+    class="container min-h-[calc(100vh-120px)] mx-auto pt-24 md:pt-30 px-3 md:px-5 pb-5 md:pb-10"
+  >
     <div class="flex">
       <slot name="sidebar"></slot>
       <slot name="content"></slot>
