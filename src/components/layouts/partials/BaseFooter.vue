9863216d4a49c422d04701a98c89c0ec434746d9
Andrey Aratov <beg431@gmail.com>
2023-08-20 11:59:56 +0700

    updated foooter

--- src/components/layouts/partials/BaseFooter.vue
+++ src/components/layouts/partials/BaseFooter.vue
@@ -1,14 +1,19 @@
 <template>
   <footer class="bg-white">
     <div
-      class="container flex flex-col items-center justify-center mx-auto p-5 text-sm text-center text-gray-500"
+      class="container flex flex-col items-center justify-center mx-auto p-5 pb-10 text-sm text-center text-gray-500"
     >
       <p>
         made with <span class="text-red-500 text-2xl">❤</span> by
         <a href="https://github.com/begprod" class="text-blue-600">begprod</a>
       </p>
       <p>
-        <a class="text-blue-600" href="#" target="_blank" rel="noreferrer">
+        <a
+          class="text-blue-600"
+          href="https://github.com/begprod/todo.it#install-application-on-your-device"
+          target="_blank"
+          rel="noreferrer"
+        >
           Install app on device instructions
           <span class="inline-block ml-2 -rotate-12 origin-center">→</span>
         </a>
