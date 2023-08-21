73e24525e9bfc6f3518b38b8c358958209df926c
Andrey Aratov <beg431@gmail.com>
2023-08-21 13:04:15 +0700

    fixed gh pages files paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todo.it/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
