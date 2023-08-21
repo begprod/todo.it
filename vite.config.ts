67973a59af655c7d7808572f563cec25111896b5
Andrey Aratov <beg431@gmail.com>
2023-08-21 12:09:41 +0700

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   plugins: [
     vue(),
@@ -19,19 +19,61 @@ export default defineConfig({
       },
       manifest: {
         short_name: 'todo.it',
-        name: '',
-        description: '',
+        name: 'todo.it – todo app with features',
+        description: 'Another todo app but with features',
         start_url: '.',
         display: 'standalone',
         orientation: 'portrait',
         theme_color: '#ffffff',
         background_color: '#ffffff',
+        categories: ['productivity', 'lifestyle', 'utilities'],
         icons: [
+          {
+            src: 'favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'favicon.ico',
+            sizes: '48x48',
+            type: 'image/x-icon',
+          },
+          {
+            src: 'android-chrome-144x144.png',
+            type: 'image/png',
+            sizes: '144x144',
+            purpose: 'any',
+          },
+          {
+            src: 'android-chrome-192x192.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'maskable',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'maskable',
+          },
+          {
+            src: 'apple-touch-icon.png',
+            type: 'image/png',
+            sizes: '180x180',
+            purpose: 'maskable',
+          },
         ],
       },
       devOptions: {
         enabled: false,
-      }
+      },
     }),
   ],
 });
