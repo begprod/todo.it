9f01671f4581db0c8cf5d47b3bbda423c6195b19
Andrey Aratov <beg431@gmail.com>
2023-08-27 16:57:44 +0700

    added generation funcs to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,27 @@
 </template>
 
 <script setup lang="ts">
+import { useCalendarStore } from '@/stores';
+import { onBeforeMount } from 'vue';
 import BaseLayoutDefault from '@/components/layouts/BaseLayoutDefault.vue';
 import BaseSidebar from '@/components/layouts/partials/BaseSidebar.vue';
 import BaseWorkSpace from '@/components/BaseWorkSpace.vue';
+
+const calendarStore = useCalendarStore();
+
+onBeforeMount(() => {
+  if (calendarStore.months.length === 0) {
+    calendarStore.setMonths();
+  }
+
+  if (Object.keys(calendarStore.tasks).length === 0) {
+    calendarStore.createTasksByDayStructure();
+  }
+
+  if (calendarStore.getIsCurrentWeekIsLast) {
+    calendarStore.setNextMonth();
+  }
+
+  calendarStore.checkAndCleanupTasksByDayStructure();
+});
 </script>
