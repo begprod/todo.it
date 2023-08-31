c68a9eba446c81d1ff500cf722494d1d00d08c83
Andrey Aratov <beg431@gmail.com>
2023-08-31 23:17:12 +0700

    updated layout component

--- src/components/layouts/BaseLayoutDefault.vue
+++ src/components/layouts/BaseLayoutDefault.vue
@@ -3,20 +3,19 @@
 
   <main class="container min-h-[calc(100vh-120px)] mx-auto p-5 lg:pb-10">
     <div class="flex flex-col lg:flex-row">
-      <slot name="sidebar"></slot>
-      <slot name="content"></slot>
+      <BaseSidebar />
+      <BaseWorkSpace />
     </div>
   </main>
 
-  <BaseTaskContextMenu :current-editing-task="calendarStore.getCurrentEditingTask" />
+  <BaseTaskContextMenu />
   <BaseFooter />
 </template>
 
 <script setup lang="ts">
-import { useCalendarStore } from '@/stores';
 import BaseHeader from '@/components/layouts/partials/BaseHeader.vue';
 import BaseFooter from '@/components/layouts/partials/BaseFooter.vue';
+import BaseSidebar from '@/components/layouts/partials/BaseSidebar.vue';
+import BaseWorkSpace from '@/components/BaseWorkSpace.vue';
 import BaseTaskContextMenu from '@/components/ui/BaseTaskContextMenu.vue';
-
-const calendarStore = useCalendarStore();
 </script>
