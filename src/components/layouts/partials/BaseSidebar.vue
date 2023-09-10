87b58cec38662cf6d1135c7a24fc034ff9e2aa25
Andrey Aratov <beg431@gmail.com>
2023-09-10 18:01:42 +0700

    feat: add sidebar animation

--- src/components/layouts/partials/BaseSidebar.vue
+++ src/components/layouts/partials/BaseSidebar.vue
@@ -1,37 +1,42 @@
 <template>
-  <div class="shrink-0 lg:mr-10 mb-10 lg:mb-0 bg-white">
+  <div class="shrink-0 lg:mr-10 mb-20 lg:mb-0 bg-white">
     <div
       class="sticky top-20 flex flex-col lg:max-h-[calc(100vh-120px)] lg:min-h-[calc(100vh-120px)] rounded-xl z-[99]"
     >
-      <BaseButton
-        v-if="!isSidebarOpen"
-        class="w-full lg:!w-12 mb-5 lg:mb-0 shrink-0"
-        title="Toggle backlog"
-        @click="toggleSidebar"
-      >
-        <div class="mr-4 lg:mr-0">
-          <v-icon name="bi-layout-sidebar-inset" />
-        </div>
-        <span class="lg:hidden">Open backlog</span>
-      </BaseButton>
-
-      <template v-if="isSidebarOpen">
-        <div class="flex gap-1 bg-white shadow-lg shadow-white z-10">
-          <BaseButton class="!w-12 mr-2 shrink-0" title="Close sidebar" @click="toggleSidebar">
-            <v-icon name="bi-layout-sidebar-inset" />
-          </BaseButton>
-          <BaseButton class="leading-none" @click="createTask('backlog')">
-            Add task to backlog
-            <template #rightIcon>
-              <div class="ml-4">
-                <v-icon name="hi-plus" />
-              </div>
-            </template>
+      <Transition>
+        <div v-if="!isSidebarOpen" class="lg:h-auto mb10 lg:mr-10">
+          <BaseButton
+            class="absolute top-0 left-0 w-full lg:!w-12 shrink-0"
+            title="Toggle backlog"
+            @click="toggleSidebar"
+          >
+            <div class="mr-4 lg:mr-0">
+              <v-icon name="bi-layout-sidebar-inset" />
+            </div>
+            <span class="lg:hidden">Open backlog</span>
           </BaseButton>
         </div>
+      </Transition>
 
-        <BaseTaskListBacklog />
-      </template>
+      <Transition>
+        <div v-if="isSidebarOpen">
+          <div class="flex gap-1 bg-white shadow-lg shadow-white z-10">
+            <BaseButton class="!w-12 mr-2 shrink-0" title="Close sidebar" @click="toggleSidebar">
+              <v-icon name="bi-layout-sidebar-inset" />
+            </BaseButton>
+            <BaseButton class="leading-none" @click="createTask('backlog')">
+              Add task to backlog
+              <template #rightIcon>
+                <div class="ml-4">
+                  <v-icon name="hi-plus" />
+                </div>
+              </template>
+            </BaseButton>
+          </div>
+
+          <BaseTaskListBacklog />
+        </div>
+      </Transition>
     </div>
   </div>
 </template>
@@ -48,3 +53,18 @@ const { isSidebarOpen } = storeToRefs(commonStore);
 const { toggleSidebar } = commonStore;
 const { createTask } = tasksStore;
 </script>
+
+<style scoped lang="scss">
+.v {
+  &-enter-active,
+  &-leave-active {
+    transition: all 0.5s ease;
+  }
+
+  &-enter-from,
+  &-leave-to {
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+}
+</style>
