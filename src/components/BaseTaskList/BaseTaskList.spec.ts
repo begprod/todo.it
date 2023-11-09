a4f5810edc4249dc024b0754ceae688d025d82f5
Andrey Aratov <beg431@gmail.com>
2023-11-09 15:56:48 +0300

    chore: update BaseTaskList test

--- src/components/BaseTaskList/BaseTaskList.spec.ts
+++ src/components/BaseTaskList/BaseTaskList.spec.ts
@@ -5,10 +5,12 @@ import { mount } from '@vue/test-utils';
 import { createTestingPinia } from '@pinia/testing';
 import { PlusIcon } from '@heroicons/vue/24/outline';
 import { useTasksStore, useCalendarStore } from '@/stores';
+import draggableComponent from 'vuedraggable';
+import BaseTaskList from '@/components/BaseTaskList/BaseTaskList.vue';
 import BaseButton from '@/components/ui/controls/BaseButton/BaseButton.vue';
 import BaseAccordion from '@/components/ui/BaseAccordion/BaseAccordion.vue';
+import BaseTask from '@/components/BaseTask/BaseTask.vue';
 import BaseEmptyListMessage from '@/components/ui/BaseEmptyListMessage/BaseEmptyListMessage.vue';
-import BaseTaskList from '@/components/BaseTaskList/BaseTaskList.vue';
 
 describe('BaseTaskList', () => {
   const wrapper = mount(BaseTaskList, {
@@ -16,8 +18,10 @@ describe('BaseTaskList', () => {
       components: {
         BaseButton,
         BaseAccordion,
+        BaseTask,
         BaseEmptyListMessage,
         PlusIcon,
+        draggableComponent,
       },
       plugins: [
         createTestingPinia({
@@ -74,6 +78,12 @@ describe('BaseTaskList', () => {
     expect(wrapper.html()).toContain('Test title');
     expect(wrapper.html()).toContain('Test description');
     expect(wrapper.html()).toContain('current-day');
+
+    expect(wrapper.findComponent(BaseAccordion).exists()).toBe(true);
+    expect(wrapper.findComponent(draggableComponent).exists()).toBe(true);
+    expect(wrapper.findComponent(BaseTask).exists()).toBe(true);
+    expect(wrapper.findComponent(BaseButton).exists()).toBe(true);
+    expect(wrapper.findComponent(BaseEmptyListMessage).exists()).toBe(false);
   });
 
   it('should render closed accordion with task list', async () => {
@@ -88,11 +98,16 @@ describe('BaseTaskList', () => {
 
     await nextTick();
 
-    expect(wrapper.findComponent(BaseEmptyListMessage).exists()).toBe(false);
     expect(wrapper.html()).toContain('October');
     expect(wrapper.html()).not.toContain('23 October');
     expect(wrapper.html()).not.toContain('Test title');
     expect(wrapper.html()).not.toContain('Test description');
+
+    expect(wrapper.findComponent(BaseAccordion).exists()).toBe(true);
+    expect(wrapper.findComponent(draggableComponent).exists()).toBe(false);
+    expect(wrapper.findComponent(BaseTask).exists()).toBe(false);
+    expect(wrapper.findComponent(BaseButton).exists()).toBe(false);
+    expect(wrapper.findComponent(BaseEmptyListMessage).exists()).toBe(false);
   });
 
   it('should render empty task list', async () => {
@@ -113,10 +128,11 @@ describe('BaseTaskList', () => {
 
     await nextTick();
 
-    expect(wrapper.findComponent(BaseEmptyListMessage).exists()).toBe(true);
     expect(wrapper.html()).toContain('October');
     expect(wrapper.html()).toContain('23 October');
     expect(wrapper.html()).toContain('No tasks for this day');
+
+    expect(wrapper.findComponent(BaseEmptyListMessage).exists()).toBe(true);
   });
 
   it('should have icon component', () => {
