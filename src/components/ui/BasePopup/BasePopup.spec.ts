15fd557dfb5496ea5ebda1718b04ea086cacae0d
Andrey Aratov <beg431@gmail.com>
2023-11-10 09:30:02 +0300

    chore: update BasePopup test

--- src/components/ui/BasePopup/BasePopup.spec.ts
+++ src/components/ui/BasePopup/BasePopup.spec.ts
@@ -12,25 +12,27 @@ describe('BasePopup', () => {
     },
   });
 
-  it('correctly showing popup when prop isVisible is true', () => {
+  it('should showing popup when prop isVisible is true', () => {
     expect(wrapper.html()).toContain('fixed bottom-0 w-full lg:items-center z-[9999]');
     expect(wrapper.html()).toContain(
       'fixed top-0 right-0 bottom-0 left-0 bg-slate-900 bg-opacity-80 z-[9998]',
     );
+
     expect(wrapper.html()).toContain('Slot content');
   });
 
-  it('correctly hiding popup when prop isVisible is false', async () => {
+  it('should hiding popup when prop isVisible is false', async () => {
     await wrapper.setProps({ isVisible: false });
 
     expect(wrapper.html()).not.toContain('fixed bottom-0 w-full lg:items-center z-[9999]');
     expect(wrapper.html()).not.toContain(
       'fixed top-0 right-0 bottom-0 left-0 bg-slate-900 bg-opacity-80 z-[9998]',
     );
+
     expect(wrapper.html()).not.toContain('Slot content');
   });
 
-  it('correctly emit click on popup backdrop', async () => {
+  it('should emit click on popup backdrop', async () => {
     await wrapper.setProps({ isVisible: true });
 
     await wrapper.find('.fixed.top-0').trigger('click');
