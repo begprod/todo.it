c8af69b34bcd6e9b6ecbfca0396624f249ce58e9
Andrey Aratov <beg431@gmail.com>
2023-12-02 15:53:25 +0300

    chore: update test
    remove snapshot test

--- src/components/layouts/partials/BaseFooter/BaseFooter.spec.ts
+++ src/components/layouts/partials/BaseFooter/BaseFooter.spec.ts
@@ -5,7 +5,18 @@ import BaseFooter from '@/components/layouts/partials/BaseFooter/BaseFooter.vue'
 describe('BaseFooter', () => {
   const wrapper = mount(BaseFooter);
 
-  it('should match snapshot BaseFooter', () => {
-    expect(wrapper.html()).toMatchSnapshot();
+  it('should contain css classes', () => {
+    expect(wrapper.html()).toContain(
+      'container flex flex-col items-center justify-center mx-auto p-5 pb-10 text-sm text-center text-gray-700',
+    );
+    expect(wrapper.html()).toContain('text-red-600 text-md');
+    expect(wrapper.html()).toContain('underline');
+    expect(wrapper.html()).toContain('inline-block ml-2 -rotate-12 origin-center');
+  });
+
+  it('should contain installation link', () => {
+    expect(wrapper.html()).toContain(
+      'https://github.com/begprod/todo.it#install-application-on-your-device',
+    );
   });
 });
