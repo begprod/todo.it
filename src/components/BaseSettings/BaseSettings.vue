a503d8727b62002190e015fa3582f0d5bcfae912
Andrey Aratov <beg431@gmail.com>
2024-08-14 14:46:53 +0300

    feat: show submit scopes, labels

--- src/components/BaseSettings/BaseSettings.vue
+++ src/components/BaseSettings/BaseSettings.vue
@@ -8,12 +8,12 @@
       </div>
 
       <div class="px-3">
-        <form id="add-scope-form" @submit.prevent="submitNewScope(newScope)">
+        <form id="add-scope-form" @submit.prevent="submitNewScope()">
           <div class="mb-2 font-bold text-neutral-600">Add label scope</div>
 
           <div class="mb-2">
             <BaseInput
-              v-model="newScope.name"
+              v-model="newScopeName"
               id="scope-name"
               placeholder="Enter scope name"
               autocomplete="off"
@@ -23,7 +23,7 @@
 
           <div class="flex flex-col items-center">
             <ColorPicker
-              v-model:pureColor="newScope.color"
+              v-model:pureColor="newScopeColor"
               class="test"
               format="hex6"
               shape="circle"
@@ -37,16 +37,21 @@
           <BaseButton class="w-full mt-2" type="submit"> Add scope </BaseButton>
         </form>
 
-        <form
-          id="add-label-form"
-          class="mt-10 mb-10"
-          @submit.prevent="submitNewLabel(newLabelData)"
-        >
+        <div v-if="getAllScopes.length > 0" class="flex flex-wrap gap-2 mb-8 mt-4">
+          <BaseLabel
+            v-for="scope in getAllScopes"
+            :key="scope.id"
+            :title="scope.name"
+            :color="scope.color"
+          />
+        </div>
+
+        <form id="add-label-form" class="mt-10 mb-10" @submit.prevent="submitNewLabel()">
           <div class="mb-2 font-bold text-neutral-600">Add label</div>
           [SCOPE SELECTOR]
           <div class="mb-2">
             <BaseInput
-              v-model="newLabelData.name"
+              v-model="newLabelName"
               id="label-name"
               placeholder="Enter label name"
               autocomplete="off"
@@ -56,6 +61,7 @@
 
           <div class="flex flex-col items-center">
             <ColorPicker
+              v-model:pureColor="newLabelColor"
               class="test"
               format="hex6"
               shape="circle"
@@ -68,6 +74,15 @@
 
           <BaseButton class="w-full mt-3" type="submit"> Add label </BaseButton>
         </form>
+
+        <div v-if="getAllLabels.length > 0" class="flex flex-wrap gap-2 mb-8 mt-4">
+          <BaseLabel
+            v-for="label in getAllLabels"
+            :key="label.id"
+            :title="label.name"
+            :color="label.color"
+          />
+        </div>
       </div>
     </template>
   </BaseSidebar>
@@ -75,36 +90,46 @@
 
 <script setup lang="ts">
 import type { IScope, ILabel } from '@/types';
-import { reactive } from 'vue';
+import uniqid from 'uniqid';
+import { ref } from 'vue';
 import { storeToRefs } from 'pinia';
 import { string } from 'yup';
 import { ColorPicker } from 'vue3-colorpicker';
 import 'vue3-colorpicker/style.css';
-import { useCommonStore } from '@/stores';
+import { useCommonStore, useLabelsStore } from '@/stores';
 import BaseSidebar from '@/components/ui/BaseSidebar/BaseSidebar.vue';
 import BaseInput from '@/components/ui/controls/BaseInput/BaseInput.vue';
 import BaseButton from '@/components/ui/controls/BaseButton/BaseButton.vue';
+import BaseLabel from '@/components/ui/BaseLabel/BaseLabel.vue';
 
 const commonStore = useCommonStore();
+const labelsStore = useLabelsStore();
 const { isSettingsOpen } = storeToRefs(commonStore);
 const { setMessage, setStatus, showToast } = commonStore;
+const { createScope, createLabel, getAllScopes, getAllLabels } = labelsStore;
 
-const newScope = reactive<IScope>({
-  name: '',
-  color: '#000000',
-});
+const newScopeName = ref<string>('');
+const newScopeColor = ref<string>('#000000');
 const newScopeSchemas = {
   name: string().required('Please enter scope name').label('Scope name'),
   color: string().required('Please choose scope color').label('Scope color'),
 };
 
-const submitNewScope = (scope: IScope) => {
+const submitNewScope = () => {
   try {
-    newScopeSchemas.name.validateSync(scope.name);
-    newScopeSchemas.color.validateSync(scope.color);
+    newScopeSchemas.name.validateSync(newScopeName.value);
+    newScopeSchemas.color.validateSync(newScopeColor.value);
 
-    newScope.name = '';
-    newScope.color = '';
+    const newScope: IScope = {
+      id: uniqid(),
+      name: newScopeName.value,
+      color: newScopeColor.value,
+    };
+
+    createScope(newScope);
+
+    newScopeName.value = '';
+    newScopeColor.value = '#000000';
   } catch (error) {
     // @ts-ignore
     setMessage(error.message);
@@ -113,25 +138,31 @@ const submitNewScope = (scope: IScope) => {
   }
 };
 
-const newLabelData = reactive<ILabel>({
-  name: '',
-  color: '#000000',
-  scope: null,
-});
+const newLabelName = ref<string>('');
+const newLabelColor = ref<string>('#000000');
+// const newLabelScope = ref<IScope | null>(null);
 const newLabelSchemas = {
   name: string().required('Please enter label name').label('Label name'),
   color: string().required('Please choose label color').label('Label color'),
 };
 
-const submitNewLabel = (label: ILabel) => {
+const submitNewLabel = () => {
   try {
-    newLabelSchemas.name.validateSync(label.name);
-    newLabelSchemas.color.validateSync(label.color);
+    newLabelSchemas.name.validateSync(newLabelName.value);
+    newLabelSchemas.color.validateSync(newLabelColor.value);
 
-    // console.log('new label', label);
+    const newLabel: ILabel = {
+      id: uniqid(),
+      name: newLabelName.value,
+      color: newLabelColor.value,
+      scope: null,
+    };
 
-    newLabelData.name = '';
-    newLabelData.color = '';
+    createLabel(newLabel);
+
+    newLabelName.value = '';
+    newLabelColor.value = '#000000';
+    // newLabelScope.value = null;
   } catch (error) {
     // @ts-ignore
     setMessage(error.message);
