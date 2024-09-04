d782285ddcf136a5388f798178428f15371cf3c9
Andrey Aratov <beg431@gmail.com>
2024-09-04 20:02:38 +0300

    chore: update input component

--- src/components/ui/controls/BaseInput/BaseInput.vue
+++ src/components/ui/controls/BaseInput/BaseInput.vue
@@ -1,21 +1,31 @@
 <template>
-  <div class="relative w-full">
+  <div class="flex items-center relative w-full">
     <input
       :id="id"
       :type="type"
       :placeholder="placeholder"
       :value="modelValue"
       :autocomplete="autocomplete"
-      :class="isError ? '!border-red-500 !focus:border-red-500' : ''"
+      :class="classes"
       :data-test-id="dataTestId"
       @input="inputHandler"
       @blur="blurHandler"
       class="w-full p-2 text-sm border border-slate-300 rounded-md hover:border-slate-400 focus:outline-none focus:border-slate-400 transition-[border-color] select-none"
     />
+
+    <div class="absolute top-1/2 -translate-y-1/2 left-2">
+      <slot name="icon-left" />
+    </div>
+
+    <div class="absolute top-1/2 -translate-y-1/2 right-2">
+      <slot name="icon-right" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed, useSlots } from 'vue';
+
 interface IProps {
   modelValue: string | number;
   id: string;
@@ -26,9 +36,11 @@ interface IProps {
   isError?: boolean;
 }
 
-defineProps<IProps>();
-
+const props = defineProps<IProps>();
 const emit = defineEmits(['update:modelValue', 'onBlur']);
+const slots = useSlots();
+const hasIconLeft = computed(() => !!slots['icon-left']);
+const hasIconRight = computed(() => !!slots['icon-right']);
 
 const inputHandler = (event: Event) => {
   emit('update:modelValue', (event.target as HTMLInputElement).value);
@@ -37,4 +49,12 @@ const inputHandler = (event: Event) => {
 const blurHandler = () => {
   emit('onBlur');
 };
+
+const classes = computed(() => {
+  return {
+    '!border-red-500 !focus:border-red-500': props.isError,
+    'pl-10': hasIconLeft.value,
+    'pr-10': hasIconRight.value,
+  };
+});
 </script>
