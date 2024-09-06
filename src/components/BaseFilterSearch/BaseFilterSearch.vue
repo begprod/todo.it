91f0826838fc91d7763f13f44059dacd3a3e6837
Andrey Aratov <beg431@gmail.com>
2024-09-06 14:12:45 +0300

    chore: add BaseFilterSearch

---
+++ src/components/BaseFilterSearch/BaseFilterSearch.vue
@@ -0,0 +1,62 @@
+<template>
+  <div v-if="searchItems.length > 0" class="relative flex items-center px-5 my-5">
+    <BaseInput
+      v-model="searchQuery"
+      :id="id"
+      :placeholder="placeholder"
+      type="text"
+      data-test-id="label-search-input"
+      autocomplete="off"
+      @on-focus="focusHandler"
+    >
+      <template #icon-left>
+        <TagIcon class="w-6 h-6 text-amber-500" />
+      </template>
+    </BaseInput>
+
+    <div class="absolute bottom-full left-0 right-0 p-5 pb-0 w-full">
+      <div class="p-5 bg-white rounded-md shadow-sm max-h-56 overflow-y-auto">
+        <BaseLabelList :labels="filteredSearchItems" @item-action="onClickItem" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { IFilterSearchItem } from '@/types';
+import { ref, computed } from 'vue';
+import { TagIcon } from '@heroicons/vue/24/outline';
+import BaseLabelList from '@/components/BaseLabelList/BaseLabelList.vue';
+import BaseInput from '@/components/ui/controls/BaseInput/BaseInput.vue';
+
+interface IProps {
+  id: string;
+  placeholder: string;
+  searchItems: Array<IFilterSearchItem>;
+}
+
+const props = defineProps<IProps>();
+const emit = defineEmits(['item-action']);
+
+const isDropDownListVisible = ref<boolean>(false);
+const searchQuery = ref<string>('');
+const filteredSearchItems = computed(() => {
+  return props.searchItems.filter((item) => {
+    if (!item.searchString) {
+      throw new Error('Search string is not defined in search item');
+    }
+
+    return item.searchString.toLowerCase().includes(searchQuery.value.toLowerCase());
+  });
+});
+
+const focusHandler = () => {
+  isDropDownListVisible.value = true;
+};
+
+const onClickItem = (item: IFilterSearchItem) => {
+  delete item.searchString;
+
+  emit('item-action', item);
+};
+</script>
