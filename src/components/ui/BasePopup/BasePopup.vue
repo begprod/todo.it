c9d48589da53832ff132850ee307536a79a7c308
Andrey Aratov <beg431@gmail.com>
2025-08-20 10:15:43 +0300

    refactor: BasePopup component

--- src/components/ui/BasePopup/BasePopup.vue
+++ src/components/ui/BasePopup/BasePopup.vue
@@ -1,26 +1,18 @@
 <template>
-  <Transition>
+  <Transition name="slide-up">
     <div
       v-if="isVisible"
-      class="fixed bottom-0 w-full lg:items-center z-[9999]"
+      class="popup"
       @keydown.esc="closeTaskActionMenu"
       data-test-id="popup-container"
     >
-      <div
-        class="min-w-full lg:min-w-[500px] p-2 text-neutral-600 rounded-md rounded-bl-none rounded-br-none bg-white z-50"
-        data-test-id="popup-content"
-      >
+      <div class="popup__inner" data-test-id="popup-content">
         <slot></slot>
       </div>
     </div>
   </Transition>
   <Transition name="fade">
-    <div
-      v-if="isVisible"
-      class="fixed top-0 right-0 bottom-0 left-0 bg-slate-900 bg-opacity-80 z-[9998]"
-      @click="$emit('close')"
-      data-testid="popup-backdrop"
-    />
+    <div v-if="isVisible" class="paranja" @click="$emit('close')" data-testid="popup-backdrop" />
   </Transition>
 </template>
 
@@ -45,46 +37,20 @@ defineExpose({
 });
 </script>
 
-<style scoped lang="scss">
-.v-enter {
-  &-active {
-    transition: all 0.5s ease;
-  }
-
-  &-from {
-    opacity: 0;
-    transform: translateY(50px);
-  }
+<style scoped>
+.popup {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  z-index: 9999;
 }
 
-.v-leave {
-  &-active {
-    transition: all 0.5s ease;
-  }
-
-  &-to {
-    opacity: 0;
-    transform: translateY(50px);
-  }
-}
-
-.fade-enter {
-  &-active {
-    transition: all 0.3s ease-out;
-  }
-
-  &-from {
-    opacity: 0;
-  }
-}
-
-.fade-leave {
-  &-active {
-    transition: all 0.3s ease-out;
-  }
-
-  &-to {
-    opacity: 0;
-  }
+.popup__inner {
+  padding: 0.5rem;
+  width: 100%;
+  background-color: var(--color-bg-surface);
+  border-radius: var(--rounded-md);
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
 }
 </style>
