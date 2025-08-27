dbe52782e3c47c02578572c967d9e97e36b8823c
Andrey Aratov <beg431@gmail.com>
2025-08-27 08:43:27 +0300

    refactor(BaseFooter): tailwind -> css

--- src/components/layouts/partials/BaseFooter/BaseFooter.vue
+++ src/components/layouts/partials/BaseFooter/BaseFooter.vue
@@ -1,23 +1,43 @@
 <template>
-  <footer
-    class="container flex flex-col items-center justify-center mx-auto p-5 text-sm text-center text-gray-700"
-    data-testid="footer"
-  >
-    <p data-testid="author-link">
-      Crafted with <span class="text-red-600 text-md">❤</span> by
-      <a href="https://github.com/begprod" class="underline">Andrej Aratov</a>
-    </p>
-    <p>
-      <a
-        class="underline"
-        href="https://github.com/begprod/todo.it#install-application-on-your-device"
-        target="_blank"
-        rel="noreferrer"
-        data-testid="installation-link"
-      >
-        Install app on device instruction
-        <span class="inline-block ml-2 -rotate-12 origin-center">→</span>
-      </a>
-    </p>
+  <footer class="footer">
+    <div>
+      Crafted with <Heart class="footer__heart icon icon_base" /> by
+      <br />
+      <a href="https://github.com/begprod" class="link link_underline">Andrej Aratov</a>
+    </div>
+
+    <a
+      class="link link_underline"
+      href="https://github.com/begprod/todo.it#install-application-on-your-device"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Install app on device instruction <ExternalLink class="icon icon_base" />
+    </a>
   </footer>
 </template>
+
+<script setup lang="ts">
+import { Heart, ExternalLink } from 'lucide-vue-next';
+</script>
+
+<style scoped>
+.footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  padding: 1rem;
+  text-align: center;
+  font-size: var(--typo-size-sm);
+
+  svg {
+    display: inline;
+  }
+}
+
+.footer__heart {
+  color: var(--color-typo-alert);
+}
+</style>
